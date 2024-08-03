45c83f09cf2ddf079ef9371f6bbf6391a75cb7ad
Piyush Jain <122745947+d3vb0ox@users.noreply.github.com>
2024-08-03 20:27:15 +0530

    add mysql settings.py

--- realworld/settings.py
+++ realworld/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,13 +81,23 @@ WSGI_APPLICATION = "realworld.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DATABASE_NAME", 'test_db'),
+        "USER": os.getenv("DATABASE_USER", 'root'),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", 'root'),
+        "HOST": os.getenv("DATABASE_HOST", '127.0.0.1'),
+        "PORT": os.getenv("DATABASE_PORT", "3306"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
